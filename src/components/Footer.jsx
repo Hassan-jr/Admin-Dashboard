2783d6abc3a7443ef3f35d845e7d3f3b3e930cf2
Hassan-jr <abdiladifhassan115@gmail.com>
2022-07-04 10:58:04 +0300

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2022 All rights reserved by hassan-jr
+      <p>Available for Work and freelancing</p>
+    </p>
+    {/* <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+     Available for Work and freelancing
+    </p> */}
+  </div>
+);
+
+export default Footer;
